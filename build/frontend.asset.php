e9ad17c10dd4024019f91b997fd32680200ddfc9
Maksym <strangerssoftware@gmail.com>
2024-11-10 22:42:22 +0200

    Upgrade packages

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'b9405a3825eb54a0dca9');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'dac41e50cc6bb6af7f78');
